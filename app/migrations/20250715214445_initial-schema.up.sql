3cc2e4ea390a470ce851d0ed204b12bad5fc39ec
dresswithpockets <dresswithpockets@pm.me>
2025-07-25 22:52:11 -0700

    start migration to postgresql

--- app/migrations/20250715214445_initial-schema.up.sql
+++ app/migrations/20250715214445_initial-schema.up.sql
@@ -1,127 +1,220 @@
-create table if not exists user
+create extension if not exists moddatetime;
+create extension if not exists pgcrypto;
+
+/*
+gen_ulid generates a ULID-based UUID: https://github.com/ulid/spec
+based on implementation here: https://web.archive.org/web/20250525070451/https://blog.daveallie.com/ulid-primary-keys/
+
+N.B.
+    this implementation of gen_ulid is very slow compared to gen_random_uuid()
+
+TODO: i'd really like to replace this with pg-ulid: https://github.com/andrielfn/pg-ulid
+*/
+create or replace function gen_ulid() returns uuid
+as
+$$
+select (lpad(to_hex(floor(extract(epoch from clock_timestamp()) * 1000)::bigint), 12, '0') ||
+        encode(gen_random_bytes(10), 'hex'))::uuid;
+$$ language sql;
+
+/*
+Example soft-delete query using deleted_record table:
+
+with deleted AS (
+    delete from user
+    where id = ?
+    returning *
+)
+insert into deleted_record(source_table, source_id, data)
+select 'user', id, to_jsonb(deleted.*)
+from deleted
+returning *;
+*/
+create table if not exists deleted_record
 (
-    id         integer primary key,
-    created_at datetime not null default (datetime('now')),
-    updated_at datetime not null,
-    deleted_at datetime,
-    slug       text     not null
+    id           uuid primary key     default gen_ulid(),
+    deleted_at   timestamptz not null default now(),
+    source_table text        not null,
+    source_id    text        not null,
+    data         jsonb       not null
 );
 
-create unique index user_unique_slug on user(slug);
-
-create table if not exists user_slug
+create table if not exists "user"
 (
-    id         integer primary key,
-    created_at datetime not null default (datetime('now')),
-    deleted_at datetime,
-    user_id    integer not null references user (id),
-    slug       text     not null
+    id         serial primary key,
+    created_at timestamptz not null default now(),
+    updated_at timestamptz not null default now(),
+    slug       text        not null unique
+);
+create trigger user_moddatetime
+    before update on "user" for each row execute procedure moddatetime(updated_at);
+
+create table if not exists user_slug_history
+(
+    id         serial primary key,
+    created_at timestamptz not null default now(),
+    user_id    integer references "user",
+    slug       text        not null
 );
 
 create table if not exists user_email
 (
-    id           integer primary key,
-    created_at   datetime not null default (datetime('now')),
-    updated_at   datetime not null,
-    deleted_at   datetime,
-    user_id      integer not null references user (id),
-    email        text     not null,
-    confirmed_at datetime
+    id           serial primary key,
+    created_at   timestamptz not null default now(),
+    updated_at   timestamptz not null default now(),
+    user_id      integer references "user",
+    email        text        not null,
+    confirmed_at timestamptz
 );
+create trigger user_email_moddatetime
+    before update on user_email for each row execute procedure moddatetime(updated_at);
 
 create table if not exists user_display_name
 (
-    id           integer primary key,
-    created_at   datetime not null default (datetime('now')),
-    deleted_at   datetime,
-    user_id      integer not null references user (id),
-    display_name text     not null
+    id           serial primary key,
+    created_at   timestamptz not null default now(),
+    user_id      integer references "user",
+    display_name text        not null
 );
 
 create table if not exists user_password
 (
-    id           integer primary key,
-    created_at   datetime not null default (datetime('now')),
-    updated_at   datetime not null,
-    user_id      integer not null references user (id),
-    encoded_hash text     not null
+    id           serial primary key,
+    created_at   timestamptz not null default now(),
+    updated_at   timestamptz not null default now(),
+    user_id      integer references "user",
+    encoded_hash text        not null
 );
+create trigger user_password_moddatetime
+    before update on user_password for each row execute procedure moddatetime(updated_at);
 
 create table if not exists developer
 (
-    id         integer primary key,
-    created_at datetime not null default (datetime('now')),
-    updated_at datetime not null,
-    deleted_at datetime,
-    slug       text     not null
+    id         serial primary key,
+    created_at timestamptz not null default now(),
+    updated_at timestamptz not null default now(),
+    slug       text        not null unique
 );
-
-create unique index developer_unique_slug on developer(slug);
+create trigger developer_moddatetime
+    before update on developer for each row execute procedure moddatetime(updated_at);
 
 create table if not exists developer_member
 (
-    id           integer primary key,
-    created_at   datetime not null default (datetime('now')),
-    deleted_at   datetime,
-    user_id      integer not null references user (id),
-    developer_id integer not null references developer (id)
+    id           serial primary key,
+    created_at   timestamptz not null default now(),
+    user_id      integer references "user",
+    developer_id integer references developer
 );
 
 create table if not exists developer_slug
 (
-    id           integer primary key,
-    created_at   datetime not null default (datetime('now')),
-    deleted_at   datetime,
-    developer_id integer not null references developer (id),
-    slug         text     not null
+    id           serial primary key,
+    created_at   timestamptz not null default now(),
+    developer_id integer references developer,
+    slug         text        not null
+);
+
+create table if not exists developer_display_name
+(
+    id           serial primary key,
+    created_at   timestamptz not null default now(),
+    developer_id integer references developer,
+    display_name text        not null
 );
 
 create table if not exists game
 (
-    id           integer primary key,
-    created_at   datetime not null default (datetime('now')),
-    updated_at   datetime not null,
-    deleted_at   datetime,
-    developer_id integer not null references developer (id),
-    slug         text     not null
-);
+    id           serial primary key,
+    created_at   timestamptz not null default now(),
+    updated_at   timestamptz not null default now(),
+    developer_id integer references developer,
+    slug         text        not null,
 
-create unique index game_unique_slug_per_dev on game(developer_id, slug);
+    unique (developer_id, slug)
+);
+create trigger game_moddatetime
+    before update on game for each row execute procedure moddatetime(updated_at);
 
 create table if not exists game_slug
 (
-    id         integer primary key,
-    created_at datetime not null default (datetime('now')),
-    deleted_at datetime,
-    game_id    integer not null references game (id),
-    slug       text     not null
+    id         serial primary key,
+    created_at timestamptz not null default now(),
+    game_id    integer references game,
+    slug       text        not null
 );
 
 create table if not exists achievement
 (
-    id                   integer primary key,
-    created_at           datetime not null default (datetime('now')),
-    updated_at           datetime not null,
-    deleted_at           datetime,
-    game_id              integer not null references game (id),
-    slug                 text     not null,
-    name                 text     not null,
-    description          text     not null,
-    progress_requirement integer  not null
+    id                   serial primary key,
+    created_at           timestamptz not null default now(),
+    updated_at           timestamptz not null default now(),
+    game_id              integer references game,
+    slug                 text        not null,
+    name                 text        not null,
+    description          text        not null,
+    progress_requirement integer     not null
 );
-
-create unique index achievement_unique_slug_per_game on achievement(game_id, slug);
 
 create table if not exists achievement_progress
 (
-    created_at     datetime not null default (datetime('now')),
-    updated_at     datetime not null,
-    deleted_at     datetime,
-    user_id        integer not null references user (id),
-    achievement_id integer not null references achievement (id),
-    progress       integer  not null,
+    created_at     timestamptz not null default now(),
+    updated_at     timestamptz not null default now(),
+    user_id        integer references "user",
+    achievement_id integer references "user",
+    progress       integer     not null,
 
     primary key (user_id, achievement_id)
 );
 
--- TODO: any other indices/constraints?
+/*
+game tokens are JWTs generated by users which include these claims:
+    iss: a resource path to the object which owns/created the slug
+         e.g user/some-user-slug
+             developer/some-developer-slug
+             developer/some-developer-slug/game/some-game-slug
+    sub: a resource path to the authorized user
+         e.g. user/some-user-slug
+    aud: a resource path to the game which this token is intended for
+         e.g. developer/some-developer-slug/game/some-game-slug
+    exp: an expiration timestamp, this is chosen by the user when they generate the token
+    nbf: always the timestamp that the token was created at
+    iat: always the timestamp that the token was created at
+    jti: a ULID 
+
+the claims are used to verify that the submitter has permission to submit achievement progress and 
+game stats for a particular user.
+
+the token table itself just stores information about issued tokens, it is not used for claims validation,
+authentication, or authorization. Only the private key & JWT claims are used for those.
+*/
+create table if not exists token
+(
+    id         uuid primary key default gen_ulid(),
+    issuer     text        not null,
+    subject    text        not null,
+    audience   text        not null,
+    expires_at timestamptz not null,
+    not_before timestamptz not null,
+    issued_at  timestamptz not null
+);
+
+/*
+the token_disallow_list table lists JWTs which have been manually expired/disallowed. Entries
+in this database are eventually deleted after now() has surpassed expire_at sufficiently.
+*/
+create table if not exists token_disallow_list
+(
+    token_id   uuid primary key references token,
+    created_at timestamptz not null default now()
+);
+
+create table if not exists game_session
+(
+    id            uuid primary key     default gen_ulid(),
+    created_at    timestamptz not null default now(),
+    token_id      uuid references token,
+    -- last time a pulse was received by the game
+    last_pulse_at timestamptz not null default now(),
+    game_id       integer references game,
+    user_id       integer references "user"
+);
