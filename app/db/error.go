6b10bc55a6163f2805235beadb8db371facb7a7a
dresswithpockets <dresswithpockets@pm.me>
2025-07-27 03:47:19 -0700

    add IsUniqueConstraintErr helper for pgconn.PgError-type constraint errors

---
+++ app/db/error.go
@@ -0,0 +1,11 @@
+package db
+
+import (
+	"errors"
+	"github.com/jackc/pgx/v5/pgconn"
+)
+
+func IsUniqueConstraintErr(err error) bool {
+	var pgErr *pgconn.PgError
+	return errors.As(err, &pgErr) && pgErr.Code == "23505"
+}
