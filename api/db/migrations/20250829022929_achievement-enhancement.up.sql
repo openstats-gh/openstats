f1a5e7f86510c8587a141fa7fb56a42afb9683bd
dresswithpockets <dresswithpockets@pm.me>
2025-09-02 16:34:55 -0700

    add unlocked_at to game_completion view

--- api/db/migrations/20250829022929_achievement-enhancement.up.sql
+++ api/db/migrations/20250829022929_achievement-enhancement.up.sql
@@ -12,6 +12,12 @@ group by a.id;
 create or replace view game_completion as
 select g.id as game_id,
        u.id as user_id,
+       (select ap1.created_at
+        from achievement_progress ap1
+             join achievement a1 on ap1.achievement_id = a1.id
+        where a1.game_id = g.id and ap1.user_id = u.id
+        order by ap1.created_at
+        limit 1) as unlocked_at,
        count(*) as unlock_count,
        count(*) = (select count(*) from achievement ga where ga.game_id = g.id) as has_every_achievement
 from achievement a
