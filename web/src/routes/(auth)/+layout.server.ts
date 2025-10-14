33958c9940e780a2c226a44f5fd44f7a05558d15
bailey <spiritov_v@pm.me>
2025-10-14 00:38:43 -0700

    restructure pages

--- web/src/routes/(auth)/+layout.server.ts
+++
@@ -1,7 +0,0 @@
-import type { LayoutServerLoad } from './$types';
-import { api } from '$lib/api';
-import {redirect} from "@sveltejs/kit";
-
-export const load: LayoutServerLoad = async ({ fetch }) => {
-
-}
