85fb32ea655f7c1c60539b60fcea9d2c0dc783ff
dresswithpockets <dresswithpockets@pm.me>
2025-10-15 09:56:56 -0700

    use new schema

--- web/src/routes/(auth)/sign-up/+page.ts
+++ web/src/routes/(auth)/sign-up/+page.ts
@@ -1,13 +1,16 @@
 import type { PageLoad } from "./$types"
 import {api} from "$lib/api";
 import {goto} from "$app/navigation";
+import {Client} from "$lib/internalApi";
+import {redirect} from "@sveltejs/kit";
 
 export const load: PageLoad = async ({ fetch }) => {
-    const session = await api.with(fetch).getCurrentSession()
 
-    if (session !== null) {
-        // redirect(307, "/")
-        await goto("/")
+    const {data, error} = await Client.GET("/internal/session/", {fetch: fetch})
+
+    if (!error) {
+        redirect(307, "/")
+        // await goto("/")
     }
 
     async function handleSubmit(event: SubmitEvent & { currentTarget: EventTarget & HTMLFormElement}) {
@@ -20,12 +23,12 @@ export const load: PageLoad = async ({ fetch }) => {
         const email = data.get("email")
         const displayName = data.get("display-name")
 
-        if (slug === null) {
+        if (!slug) {
             console.error("slug is missing")
             return
         }
 
-        if (password === null) {
+        if (!password) {
             console.error("password is missing")
             return
         }
@@ -33,7 +36,15 @@ export const load: PageLoad = async ({ fetch }) => {
         let emailValue = email?.toString()
         let displayNameValue = displayName?.toString()
 
-        const result = await api.with(fetch).signUp(slug.toString(), password.toString(), emailValue, displayNameValue)
+        const {error} = await Client.POST("/internal/session/sign-up", {
+            fetch: fetch,
+            body: {
+                email: emailValue?.toString() ?? "",
+                displayName: displayNameValue?.toString() ?? "",
+                slug: slug.toString(),
+                password: password.toString(),
+            }
+        })
 
         // TODO: show result
 
