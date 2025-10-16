346b5fd621ac3246d95e10b8f6d523666cfff266
spiritov <spiritov_v@pm.me>
2025-10-16 13:51:32 -0700

    handle sign-out (#17)

--- web/src/routes/(app)/+layout.server.ts
+++ web/src/routes/(app)/+layout.server.ts
@@ -1,11 +1,11 @@
-import { redirect } from "@sveltejs/kit";
+// import { redirect } from "@sveltejs/kit";
 import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = ({ locals }) => {
   if (!locals.session) {
-    redirect(302, "/welcome");
+    // todo: redirect here if not the main page, not sign-in
+    // redirect(302, "/");
   }
-
   return {
     session: locals.session,
   };
