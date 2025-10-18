fe99f55967871d7987453b406c86501ba7dfda8a
bailey <spiritov_v@pm.me>
2025-10-17 20:14:42 -0700

    update achievement component

--- web/src/lib/components/Achievement.svelte
+++ web/src/lib/components/Achievement.svelte
@@ -2,7 +2,7 @@
   let { src, name, desc = "" }: { src: string; name: string; desc?: string } = $props();
 </script>
 
-<div class="flex">
+<div class="border-1 flex w-64 shrink-0 items-center gap-2 border-zinc-700 bg-zinc-900 p-2">
   <img {src} alt="" class="size-16" />
-  <span>{name}</span>
+  <span class="overflow-hidden overflow-ellipsis text-nowrap">{name}</span>
 </div>
