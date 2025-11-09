894d6dcb640d8cb29a51098f2fcb214dddffef79
bailey <spiritov_v@pm.me>
2025-11-08 18:13:17 -0700

    remove dummy components

--- web/src/lib/components/Achievement.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  let { src, name, desc = "" }: { src: string; name: string; desc?: string } = $props();
-</script>
-
-<div class="border-1 flex w-64 shrink-0 items-center gap-2 border-zinc-700 bg-zinc-900 p-2">
-  <img {src} alt="" class="size-16" />
-  <span class="overflow-hidden overflow-ellipsis text-nowrap">{name}</span>
-</div>
